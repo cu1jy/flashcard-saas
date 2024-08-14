53467aa32d1ca6e6d4ebe5e273d6d94b6596f962
Flora Dedvukaj <floradedvukaj@Floras-MacBook-Pro.local>
2024-08-14 15:26:18 -0400

    stripe integration

--- flashcard-saas/src/flashcards/page.js
+++ flashcard-saas/src/flashcards/page.js
@@ -28,6 +28,23 @@ export default function Flashcard() {
     }, [user])
 }
 
+const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_sessions', {
+        method: 'POST',
+        headers : { origin: 'http://localhost:3000'},
+    })
+    const checkoutSessionJson = await checkoutSession.json()
+
+    const stripe = await getStripe()
+    const {error} = await stripe.redirectToCheckout({
+        sessionId: checkoutSessionJson.id,
+    })
+
+    if (error) {
+        console.warn(error.message)
+    }
+}
+
 return (
     <Container maxWidth = "md">
         <Grid container spacing = {3} sx={{mt : 4}}>
