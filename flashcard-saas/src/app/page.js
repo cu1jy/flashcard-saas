b7cb394592c3f912ea4fbc5180ca19ffe619713a
Hannah Mendbayar <cmendbayar@middlebury.edu>
2024-08-15 17:52:44 -0500

    Middleware-fixed

--- flashcard-saas/src/app/page.js
+++ flashcard-saas/src/app/page.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import getStripe from "../../utils/get-stripe";
 import { SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
+import { Container, AppBar, Toolbar, Typography, Button } from '@mui/material';
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -20,7 +22,6 @@ export default function Home() {
           <SignedIn>
             <UserButton />
           </SignedIn>
-          
         </Toolbar>
       </AppBar>
     </Container>  
